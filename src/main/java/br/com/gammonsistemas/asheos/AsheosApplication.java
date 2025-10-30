7b8d945c9ac6714677ba0345539ed8e9533b96dc
Hugo Henrique <hugo.hbs64@gmail.com>
2025-10-30 15:21:00 -0300

    Projeto gerado por Spring Initializr

---
+++ src/main/java/br/com/gammonsistemas/asheos/AsheosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gammonsistemas.asheos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsheosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsheosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gammonsistemas/asheos/AsheosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gammonsistemas.asheos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsheosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
