99baa45137d41b423f9e9535fca7812e27bfd1f4
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-04 15:26:44 -0300

    feat(auth): camada de service para autenticação de usuário; adicionando função builder no TokenResponse; adicionando busca de existe por email no UserRepository.

---
+++ src/main/java/br/com/gammonsistemas/asheos/domain/auth/AuthService.java
@@ -0,0 +1,73 @@
+package br.com.gammonsistemas.asheos.domain.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.gammonsistemas.asheos.core.security.TokenService;
+import br.com.gammonsistemas.asheos.domain.auth.dto.LoginRequest;
+import br.com.gammonsistemas.asheos.domain.auth.dto.RegisterRequest;
+import br.com.gammonsistemas.asheos.domain.auth.dto.TokenResponse;
+import br.com.gammonsistemas.asheos.domain.user.User;
+import br.com.gammonsistemas.asheos.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+
+    /**
+     * Registra um novo usuário
+     * 
+     * @return Token JWT
+     */
+    public TokenResponse register(RegisterRequest request) {
+
+        // Verifica se o email já está em uso
+        if (userRepository.existsByEmail(request.email())) {
+            throw new IllegalArgumentException("O email já está em uso");
+        }
+
+        User user = new User(
+                null,
+                request.name(),
+                request.email(),
+                passwordEncoder.encode(request.password()));
+
+        user = userRepository.save(user);
+
+        // Gera o token JWT
+        String token = tokenService.generateToken(user);
+
+        return TokenResponse.builder().token(token).build();
+    }
+
+    /**
+     * Autentica o usuário
+     * 
+     * @return Token JWT
+     */
+    public TokenResponse login(LoginRequest request) {
+        // Autentica o usuário
+        authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.email(),
+                                request.password()));
+
+        UserDetails user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new RuntimeException("Credenciais inválidas"));
+
+        // Gera o token JWT
+        String token = tokenService.generateToken(user);
+
+        return TokenResponse.builder().token(token).build();
+    }
+}
--- src/main/java/br/com/gammonsistemas/asheos/domain/auth/dto/TokenResponse.java
+++ src/main/java/br/com/gammonsistemas/asheos/domain/auth/dto/TokenResponse.java
@@ -1,5 +1,8 @@
 package br.com.gammonsistemas.asheos.domain.auth.dto;
 
+import lombok.Builder;
+
+@Builder
 public record TokenResponse(
         String token,
         String type) {
