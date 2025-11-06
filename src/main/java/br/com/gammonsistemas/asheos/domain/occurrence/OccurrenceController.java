8628845fc80e7ce150b004426d77d7dbfb30fba7
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-06 15:43:30 -0300

    feat(occurrence): adicionando métodos de atualizar e excluir no OccurrenceController

--- src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/OccurrenceController.java
+++ src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/OccurrenceController.java
@@ -6,11 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gammonsistemas.asheos.domain.occurrence.dto.OccurrenceRequest;
@@ -49,4 +52,16 @@ public class OccurrenceController {
         return ResponseEntity.status(HttpStatus.CREATED).body(occurrence);
     }
 
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Occurrence> patchOccurrenceStatus(@PathVariable Long id,
+            @RequestParam OccurrenceStatus status) {
+        Occurrence occurrence = occurrenceService.update(id, status);
+        return ResponseEntity.ok(occurrence);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteOccurrence(@PathVariable Long id) {
+        occurrenceService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
