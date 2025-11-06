63e0c6ce966a3e039c22c82fe92b3c165e78c07b
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-06 00:39:01 -0300

    test(occurrence): teste unitário de repositório de ocorrência; fix(occurrence): corrigindo falta de @Id na entidade Occurrence

--- src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/Occurrence.java
+++ src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/Occurrence.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Occurrence {
     
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
