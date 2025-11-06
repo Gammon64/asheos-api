d67f7522020ba93b5ec0d23298f27125d7fc42b2
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-06 15:14:13 -0300

    fix(occurrence): adicionando @Repository em OccurrenceRepository

--- src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/OccurrenceRepository.java
+++ src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/OccurrenceRepository.java
@@ -3,7 +3,9 @@ package br.com.gammonsistemas.asheos.domain.occurrence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OccurrenceRepository extends JpaRepository<Occurrence, Long> {
     
     List<Occurrence> findByReportedBy_id(Long id);
