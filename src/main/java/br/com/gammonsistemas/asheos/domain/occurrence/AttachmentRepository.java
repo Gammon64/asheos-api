7cd8943cdeef745387a687733271ac560aadf4de
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-07 14:52:10 -0300

    feat(occurrence): função buscar anexos da ocorrência no repositório

--- src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/AttachmentRepository.java
+++ src/main/java/br/com/gammonsistemas/asheos/domain/occurrence/AttachmentRepository.java
@@ -1,9 +1,13 @@
 package br.com.gammonsistemas.asheos.domain.occurrence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+    List<Attachment> findAllByOccurrence_Id(Long occurrenceId);
     
 }
