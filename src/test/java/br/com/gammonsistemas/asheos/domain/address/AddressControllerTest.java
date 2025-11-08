f198ac30b0195087c7647589eb228fabb21ba5a5
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-08 14:31:16 -0300

    test(occurrence): adicionando teste para listagem, atualização e exclusão de endereço

--- src/test/java/br/com/gammonsistemas/asheos/domain/address/AddressControllerTest.java
+++ src/test/java/br/com/gammonsistemas/asheos/domain/address/AddressControllerTest.java
@@ -4,7 +4,10 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,67 +29,145 @@ import br.com.gammonsistemas.asheos.domain.user.UserRepository;
 
 public class AddressControllerTest extends AbstractRestDocsTest {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private AddressRepository addressRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+        @Autowired
+        private ObjectMapper objectMapper;
+        @Autowired
+        private UserRepository userRepository;
+        @Autowired
+        private AddressRepository addressRepository;
+        @Autowired
+        private PasswordEncoder passwordEncoder;
 
-    private User mockLoggedUser;
+        private User mockLoggedUser;
 
-    @BeforeEach
-    void setUp() {
-        addressRepository.deleteAll();
-        userRepository.deleteAll();
+        @BeforeEach
+        void setUp() {
+                addressRepository.deleteAll();
+                userRepository.deleteAll();
 
-        // Cria o usuário no banco
-        mockLoggedUser = UserMock.USER_JOHN_DOE();
-        mockLoggedUser.setPassword(passwordEncoder.encode(UserMock.USER_PASSWORD));
+                // Cria o usuário no banco
+                mockLoggedUser = UserMock.USER_JOHN_DOE();
+                mockLoggedUser.setPassword(passwordEncoder.encode(UserMock.USER_PASSWORD));
 
-        userRepository.save(mockLoggedUser);
-    }
+                userRepository.save(mockLoggedUser);
+        }
 
-    @Test
-    @WithMockUser(username = "johndoe@test.com")
-    @DisplayName("Deve criar um endereço")
-    void testCreateAddress() throws Exception {
-        // Given
-        AddressRequest request = new AddressRequest(
-                AddressMock.ADDRESS_STREET,
-                AddressMock.ADDRESS_CITY,
-                AddressMock.ADDRESS_STATE,
-                AddressMock.ADDRESS_ZIP_CODE);
-        String jsonRequest = objectMapper.writeValueAsString(request);
+        @Test
+        @WithMockUser(username = "johndoe@test.com")
+        @DisplayName("Deve criar um endereço")
+        void testCreateAddress() throws Exception {
+                // Given
+                AddressRequest request = new AddressRequest(
+                                AddressMock.ADDRESS_STREET,
+                                AddressMock.ADDRESS_CITY,
+                                AddressMock.ADDRESS_STATE,
+                                AddressMock.ADDRESS_ZIP_CODE);
+                String jsonRequest = objectMapper.writeValueAsString(request);
 
-        // When & Then
-        mockMvc.perform(post("/addresses")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonRequest))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.street").value(AddressMock.ADDRESS_STREET))
-                .andExpect(jsonPath("$.user.id").value(mockLoggedUser.getId()))
-                .andExpect(jsonPath("$.user.password").doesNotExist())
-                .andDo(document("addresses/create",
-                        requestFields(
-                                fieldWithPath("street").description("Nome da rua."),
-                                fieldWithPath("city").description("Nome da cidade."),
-                                fieldWithPath("state").description("Sigla do estado (2 caracteres)."),
-                                fieldWithPath("zipCode").description("CEP (Código Postal).")),
-                        responseFields(
-                                fieldWithPath("id").description("ID do endereço criado."),
-                                fieldWithPath("street").description("Nome da rua."),
-                                fieldWithPath("city").description("Nome da cidade."),
-                                fieldWithPath("state").description("Sigla do estado."),
-                                fieldWithPath("zipCode").description("CEP."),
-                                fieldWithPath("user")
-                                        .description("Objeto do usuário associado (omitindo dados sensíveis)."),
-                                fieldWithPath("user.id").description("ID do usuário."),
-                                fieldWithPath("user.name").description("Nome do usuário."),
-                                fieldWithPath("user.email").description("Email do usuário."))));
-    }
+                // When & Then
+                mockMvc.perform(post("/addresses")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonRequest))
+                                .andExpect(status().isCreated())
+                                .andExpect(jsonPath("$.id").exists())
+                                .andExpect(jsonPath("$.street").value(AddressMock.ADDRESS_STREET))
+                                .andDo(document("addresses/create",
+                                                requestFields(
+                                                                fieldWithPath("street").description("Nome da rua."),
+                                                                fieldWithPath("city").description("Nome da cidade."),
+                                                                fieldWithPath("state").description(
+                                                                                "Sigla do estado (2 caracteres)."),
+                                                                fieldWithPath("zipCode")
+                                                                                .description("CEP (Código Postal).")),
+                                                responseFields(
+                                                                fieldWithPath("id")
+                                                                                .description("ID do endereço criado."),
+                                                                fieldWithPath("street").description("Nome da rua."),
+                                                                fieldWithPath("city").description("Nome da cidade."),
+                                                                fieldWithPath("state").description("Sigla do estado."),
+                                                                fieldWithPath("zipCode").description("CEP."))));
+        }
 
+        @Test
+        @WithMockUser(username = "johndoe@test.com")
+        @DisplayName("Deve listar os endereços do usuário logado")
+        void testGetAddressesByUser() throws Exception {
+                // Given
+                Address address = AddressMock.ADDRESS_JOHN_DOE();
+                address.setUser(mockLoggedUser);
+                addressRepository.save(address);
+
+                // When & Then
+                mockMvc.perform(get("/addresses"))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$[0].id").exists())
+                                .andExpect(jsonPath("$[0].street").value(AddressMock.ADDRESS_STREET))
+                                .andDo(document("addresses/list",
+                                                responseFields(
+                                                                fieldWithPath("[]").description(
+                                                                                "Lista de endereços."),
+                                                                fieldWithPath("[].id")
+                                                                                .description("ID do endereço criado."),
+                                                                fieldWithPath("[].street").description("Nome da rua."),
+                                                                fieldWithPath("[].city").description("Nome da cidade."),
+                                                                fieldWithPath("[].state")
+                                                                                .description("Sigla do estado."),
+                                                                fieldWithPath("[].zipCode").description("CEP."))));
+        }
+
+        @Test
+        @WithMockUser(username = "johndoe@test.com")
+        @DisplayName("Deve atualizar um endereço")
+        void testUpdateAddress() throws Exception {
+                // Given
+                Address address = AddressMock.ADDRESS_JOHN_DOE();
+                address.setUser(mockLoggedUser);
+                addressRepository.save(address);
+
+                AddressRequest request = new AddressRequest(
+                                "Rua 09",
+                                AddressMock.ADDRESS_CITY,
+                                AddressMock.ADDRESS_STATE,
+                                AddressMock.ADDRESS_ZIP_CODE);
+                String jsonRequest = objectMapper.writeValueAsString(request);
+
+                // When & Then
+                mockMvc.perform(put("/addresses/{id}", address.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonRequest))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.id").exists())
+                                .andExpect(jsonPath("$.street").value("Rua 09"))
+                                .andDo(document("addresses/update",
+                                                requestFields(
+                                                                fieldWithPath("street").description("Nome da rua."),
+                                                                fieldWithPath("city").description("Nome da cidade."),
+                                                                fieldWithPath("state").description(
+                                                                                "Sigla do estado (2 caracteres)."),
+                                                                fieldWithPath("zipCode")
+                                                                                .description("CEP (Código Postal).")),
+                                                responseFields(
+                                                                fieldWithPath("id")
+                                                                                .description("ID do endereço criado."),
+                                                                fieldWithPath("street").description("Nome da rua."),
+                                                                fieldWithPath("city").description("Nome da cidade."),
+                                                                fieldWithPath("state").description("Sigla do estado."),
+                                                                fieldWithPath("zipCode").description("CEP."))));
+        }
+
+        @Test
+        @WithMockUser(username = "johndoe@test.com")
+        @DisplayName("Deve excluir um endereço")
+        void testDeleteAddress() throws Exception {
+                // Given
+                Address address = AddressMock.ADDRESS_JOHN_DOE();
+                address.setUser(mockLoggedUser);
+                addressRepository.save(address);
+
+                // When & Then
+                mockMvc.perform(
+                                delete("/addresses/{id}", address.getId()))
+                                .andExpect(status().isNoContent()) // Espera 204
+                                .andDo(document("addresses/delete"));
+        }
 }
