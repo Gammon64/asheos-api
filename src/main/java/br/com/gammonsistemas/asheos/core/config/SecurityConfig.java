337a1d56a3fb30e1fcfd1dbbacaa7fd0994223c4
Hugo Henrique <hugo.hbs64@gmail.com>
2025-11-20 08:46:09 -0300

    fix(infra):  a rota /auth/login no SecurityConfig estava faltando uma '/'

--- src/main/java/br/com/gammonsistemas/asheos/core/config/SecurityConfig.java
+++ src/main/java/br/com/gammonsistemas/asheos/core/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 // Define regras de autorização para requisições
                 .authorizeHttpRequests(auth -> auth
                         // Libera rotas de autenticação
-                        .requestMatchers(HttpMethod.POST, "/auth/register", "auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
                         // Obriga autenticação para outras rotas
                         .anyRequest().authenticated())
                 // Define o provedor de autenticação
